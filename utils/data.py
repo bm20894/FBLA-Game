a43f4a770d9cc95990a9b73893f80c62ab917349
Miles <41974447+bm20894@users.noreply.github.com>
2019-03-06 10:26:38 -0600

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -63,5 +63,111 @@ data = [
                 "C": -1,
                 "D": 1
         }
+},
+{
+    "text": "How can a business raise funds?\n\nA: Angel Investors\nB: Crowd Funding\nC: Bank Loan\nD: All of the Above",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": -1,
+                "D": 1
+        }
+},
+{
+    "text": "The accounting term \"in the red\" means the business:\n\nA: Lost Money\nB: Earned Money\nC: Is in bankruptcy",
+        "buttons": {
+                "A": 1,
+                "B": -1,
+                "C": -1
+        }
+},
+{
+    "text": "Monpoly is:\n\nA: A board game from the 1930's\nB: A market with only one supplier\nC: Both",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": 1
+        }
+},
+{
+    "text": "Which of the following is an example of proper business attire?\n\nA: Sneakers\nB: Backwards Baseball hat\nC: Suit and tie\nD: All of the above",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": 1,
+                "D": -1
+        }
+},
+{
+    "text": "Advertising plans are based on:\n\nA: Objectives that describe the desired result\nB: Emotional instincts\nC: Company budgets\nD: Entreprenuership",
+        "buttons": {
+                "A": 1,
+                "B": -1,
+                "C": -1,
+                "D": -1
+        }
+},
+{
+    "text": "The assortment of products that a business sells is called the:\n\nA: Product mix\nB: Product assortment\nC: Product reach\nD: Product line",
+        "buttons": {
+                "A": 1,
+                "B": -1,
+                "C": -1,
+                "D": -1
+        }
+},
+{
+    "text": "The point at which supply and demand are equal is known as the:\n\nA: Elastic demand\nB: Inelastic demand\nC: Economic balance\nD: Price equilibrium",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": -1,
+                "D": 1
+        }
+},
+{
+    "text": "This is an example of malware for profit.\n\nA: Man-In-The-Middle\nB: Honeypot\nC: Rootkit\nD: Ransomware",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": -1,
+                "D": 1
+        }
+},
+{
+    "text": "Call back is an example of what type of security?\n\nA: Scanner\nB: Filter\nC: Encryption\nD: Remote Access",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": -1,
+                "D": 1
+        }
+},
+{
+    "text": "A demilitarized zone (DMZ) refers to a portion of the network that is not fully trusted. What type of network zones do administrators use when creating a DMZ?\n\nA: Securely protected\nB: Fully-protected\nC: Semi-protected\nD: decentralized",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": 1,
+                "D": -1
+        }
+},
+{
+    "text": "Which technology issues a challenge/response test as a means of ascertaining that a user is a human and not a computer program?\n\nA: Munging\nB: Site Key\nC: CHAP\nD: CAPTCHA",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": -1,
+                "D": 1
+        }
+},
+{
+    "text": "To improve communication among diverse workplace audiences, someone would NOT:\n\nA: Find commonalities\nB: Make a lot of assumptions\nC: Understanding the values of differences\nD: Build on similarities",
+        "buttons": {
+                "A": -1,
+                "B": 1,
+                "C": -1,
+                "D": -1
+        }
 }
 ]
