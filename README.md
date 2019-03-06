d97d42ec84dbad0fb64c7ed4751eb191f86862d3
bm20894 <milesboz@me.com>
2019-03-05 18:19:23 -0600

    real questions

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ So far, I have experimented with different types of sprite movement, physics, an
 * `wall_example.py` shows a relationship between two sprites: the player and a wall. A "physics engine" is used to make sure the player can not move through the wall.
 * `ball.py` is a simulation of a bouncing ball. No "physics engine" was used in this script, just variables storing values like Gravity Constants and the bounciness of the ball.
 * `game.py` is the most complete program so far. It features multiple different levels, with some unique differences between them. There is also a "Game Over" screen after the player has collected all of the coins, with an option to play again.
+
+## Installation
+- *EXE*: Have a double-clickable file that runs the game with no terminal or dependancy installations
+- *Pip*: Use a shell script for windows that installs the packagage from PYPI (pip install FBLA-Game)
+	* Script could look something like this:
+
+		#!/bin/bash
+		pip install arcade FBLA-Game
+		python3 -m FBLA-Game
--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,45 @@
 data = [
 {
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "text": "P&L stands for:\nA: Process and Lags\nB: Pass-Through Income and Liability\nC: Profit and Loss",
+        "buttons": {
+                "A": -1,
+                "B": -1,
+                "C": 1
+        }
+},
+{
+    "text": "True or false: Net revenue is sales minus costs.",
+        "buttons": {
+                "True": 1,
+                "False": -1
+        }
+},
+{
+    "text": "A type of legal business entity is a:\nA: LNC\nB: LLC\nC: BNC\nD: L&E",
+        "buttons": {
+                "A": -1,
+                "B": 1,
+                "C": -1,
+                "D": -1
+        }
+},
+{
+    "text": "A source of investment funding for your business is:\nA: VC / Venture Capital\nB: Anglers\nC: 529 Loan",
+        "buttons": {
+                "A": 1,
+                "B": -1,
+                "C": -1
+        }
+},
+{
+    "text": "True or False: Marketing is how you communicate a brand's products and services.",
+        "buttons": {
+                "True": 1,
+                "False": -1,
+        }
+},
+{
+    "text": "Which of the following is issued to employees for tax filing?\nA: W2\nB: 401k\nC: 1099\nD: 5013c",
         "buttons": {
                 "A": 1,
                 "B": -1,
@@ -9,26 +48,20 @@ data = [
         }
 },
 {
-        "text": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "text": "Product margin is calculated from the ratio of:\nA: Price and Cost\nB: Revenue minus Returns\nC: Sales minus taxes",
         "buttons": {
-                "yes": -2,
-                "no": 2
+                "A": 1,
+                "B": -1,
+                "C": -1
         }
 },
 {
-        "text": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "text": "Inventory flow is best managed:\nA: Quarterly\nB: Seasonally\nC: Up front\nD: Based on demand",
         "buttons": {
                 "A": -1,
                 "B": -1,
-                "C": 1,
-                "D": -1
-        }
-},
-{
-        "text": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "buttons": {
-                "yes": 2,
-                "no": -2
+                "C": -1,
+                "D": 1
         }
 }
 ]
